1f3deca6fb42560808ed0bc2dbbbb3cd7a449b7d
SaliMerc <nyawadesaline@gmail.com>
2025-07-14 10:55:32 +0300

    mod debug status

--- LughaNestBackend/settings.py
+++ LughaNestBackend/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m0oqivi1$3o19^l$lu6gfum35n-y@(k@0t$kw_k)#px_w@&o04'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG_STATUS')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'lughanest-backend-apis.onrender.com']
 
 
 # Application definition
--- payment_app/urls.py
+++ payment_app/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('lipa-na-mpesa/', LipaNaMpesaOnlineAPIView.as_view(), name='lipa-na-mpesa'),
-    path('lipa-na-mpesa-callback/', MpesaCallbackAPIView.as_view(), name='lipa-na-mpesa-callback'),
+    path('callback/', MpesaCallbackAPIView.as_view(), name='lipa-na-mpesa-callback'),
     path('payment-status/', PaymentStatusAPIView.as_view(), name='payment-status'),
 ]
