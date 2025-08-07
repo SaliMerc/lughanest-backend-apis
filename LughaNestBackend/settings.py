55636f88952e5d2042f5823bc2677534047e3eef
SaliMerc <nyawadesaline@gmail.com>
2025-08-07 16:49:53 +0300

    modified channels settnng

--- LughaNestBackend/settings.py
+++ LughaNestBackend/settings.py
@@ -165,27 +165,44 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 """Channels setup"""
 ASGI_APPLICATION = 'LughaNestBackend.asgi.application'  
 
+ASGI_THREADS = 100
+
+CHANNELS_WEBSOCKET_TIMEOUT = 1800
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",   
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [("localhost", 6379)],
-            "symmetric_encryption_keys": [SECRET_KEY[:32]], 
+            "symmetric_encryption_keys": [SECRET_KEY[:32]],
+            "capacity": 1000,
+            "expiry": 60,
             "channel_capacity": {
-                "http.request": 200,  
+                "http.request": 200,
                 "http.response*": 100,
-                re.compile(r"^user_\d+"): 50,    
-                re.compile(r"^chat_\d+_\d+"): 100, 
-            },
-            "connection_kwargs": {
-                "socket_keepalive": True,
-                "socket_connect_timeout": 5,
-                "retry_on_timeout": True,
+                re.compile(r"^user_\d+"): 50,
+                re.compile(r"^chat_\d+_\d+"): 100,
             },
         },
     }
 }
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",   
+#         "CONFIG": {
+#             "hosts": [("localhost", 6379)],
+#             "symmetric_encryption_keys": [SECRET_KEY[:32]], 
+#             "channel_capacity": {
+#                 "http.request": 200,  
+#                 "http.response*": 100,
+#                 re.compile(r"^user_\d+"): 50,    
+#                 re.compile(r"^chat_\d+_\d+"): 100, 
+#             },
+#         },
+#     }
+# }
+
 
 ROOT_URLCONF = 'LughaNestBackend.urls'
 
